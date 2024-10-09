58349fdc93846d16688f88969a4be28dd780e24d
gavin-gallant <gavin.gallant@mths.ca>
2024-10-09 10:40:41 -0400

    fixed code

--- main.ts
+++ main.ts
@@ -22,14 +22,17 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Square)
     // Green
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
     pause(1000)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     // Yellow
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
     pause(500)
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     // Red
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
     pause(1000)
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
